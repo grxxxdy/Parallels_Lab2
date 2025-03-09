3dfec906105e78f886821d3280ee4f2e586513ed
grxxxdy <mozjukhin@gmail.com>
2025-03-09 13:35:01 +0200

    Linear solution

---
+++ Parallel_Lab2/NumList.cs
@@ -0,0 +1,48 @@
+﻿namespace Parallel_Lab2;
+
+public class NumList
+{
+    private List<int> _nums;
+    private int _counter;
+
+    public NumList(int size)
+    {
+        _nums = new List<int>(size);
+        _counter = 0;
+    }
+
+    public void FillWithRands()
+    {
+        Random rand = new Random();
+        
+        for (int i = 0; i < _nums.Capacity; i++)
+        {
+            _nums.Add(rand.Next(0, 10001));
+        }
+    }
+
+    public int CountBiggerNumbers(int numToCompare)
+    {
+        foreach (var num in _nums)
+        {
+            if (num > numToCompare)
+            {
+                _counter++;
+            }
+        }
+
+        return _counter;
+    }
+
+    public int FindMax()
+    {
+        int maxNum = -1;
+        
+        foreach (var num in _nums)
+        {
+            if (num > maxNum) maxNum = num;
+        }
+
+        return maxNum;
+    }
+}
--- Parallel_Lab2/Program.cs
+++ Parallel_Lab2/Program.cs
@@ -1,9 +1,45 @@
-﻿namespace Parallel_Lab2;
+﻿using System.Diagnostics;
+
+namespace Parallel_Lab2;
 
 class Program
 {
     static void Main(string[] args)
     {
+        const int numsSize = 100000000;
+        const int numToCompare = 101;
+
+        NumList nums = new NumList(numsSize);
+        Stopwatch stopwatch = new Stopwatch();
+        
+        stopwatch.Start();
+        nums.FillWithRands();
+        stopwatch.Stop();
+
+        long fillTime = stopwatch.ElapsedMilliseconds;
+        stopwatch.Reset();
+        
+        stopwatch.Start();
+        int count = nums.CountBiggerNumbers(numToCompare);
+        stopwatch.Stop();
+        
+        long countTime = stopwatch.ElapsedMilliseconds;
+        stopwatch.Reset();
+        
+        stopwatch.Start();
+        int max = nums.FindMax();
+        stopwatch.Stop();
+        
+        long searchTime = stopwatch.ElapsedMilliseconds;
+        stopwatch.Reset();
+        
+        Console.WriteLine($"List size: {numsSize}\n");
+        Console.WriteLine($"Fill time: {fillTime}");
+        Console.WriteLine($"Count time: {countTime}");
+        Console.WriteLine($"Search time: {searchTime}");
+        Console.WriteLine($"Total time: {fillTime + countTime + searchTime}\n");
+        Console.WriteLine($"Elements bigger than {numToCompare}: {count}");
+        Console.WriteLine($"Max number: {max}");
         
     }
 }
